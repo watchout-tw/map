066a71b6540ea9e26862edbf89df0fdcb13f8ada
chihao <chihaoyo@gmail.com>
2017-05-27 16:29:44 +0800

    GET data

--- 748/src/app.js
+++ 748/src/app.js
@@ -1,15 +1,17 @@
-Vue.component('world', {
-  created: function() {
-
-  },
-  template: `
-  <div class="map map-world">map</div>
-  `
-});
-
 var app = new Vue({
   el: '#app',
   methods: {
+    getSuccess: function(response) {
+      var rows = response.body;
+      console.log(rows);
+    },
+    getError: function(response) {
+      console.error(response);
+    },
+    markdown: marked,
+  },
+  created: function() {
+    Vue.http.get('./src/data.json').then(this.getSuccess, this.getError);
   },
   data: {
     common: CommonData,
@@ -35,8 +37,5 @@ var app = new Vue({
       title: '結論：建國尚未成功',
       description: '在沃草，我們持續以各種方式，**努力降低理解複雜議題的門檻**。像《釋憲748號》這樣的資訊新聞需要許多人力整理資料、設計、製作，如果你喜歡沃草的內容，請別忘了[支持我們](https://watchout.tw/#support)！',
     },
-  },
-  methods: {
-    markdown: marked,
   },
 });
