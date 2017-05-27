6e730f1e3c1738f6c09c644b8f81f9f78ea12280
chihao <chihaoyo@gmail.com>
2017-05-27 16:10:51 +0800

    Add index.html & app.js

---
+++ 748/src/app.js
@@ -0,0 +1,42 @@
+Vue.component('world', {
+  created: function() {
+
+  },
+  template: `
+  <div class="map map-world">map</div>
+  `
+});
+
+var app = new Vue({
+  el: '#app',
+  methods: {
+  },
+  data: {
+    common: CommonData,
+    header: {
+      title: '釋字748號',
+      description: '',
+    },
+    authorship: [
+      {
+        job: '資料分析',
+        people: ['洪國鈞', '游知澔'],
+      },
+      {
+        job: '編輯',
+        people: ['洪國鈞'],
+      },
+      {
+        job: '設計開發',
+        people: ['游知澔'],
+      },
+    ],
+    conclusion: {
+      title: '結論：建國尚未成功',
+      description: '在沃草，我們持續以各種方式，**努力降低理解複雜議題的門檻**。像《釋憲748號》這樣的資訊新聞需要許多人力整理資料、設計、製作，如果你喜歡沃草的內容，請別忘了[支持我們](https://watchout.tw/#support)！',
+    },
+  },
+  methods: {
+    markdown: marked,
+  },
+});
